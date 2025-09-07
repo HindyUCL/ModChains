a79847f62c6caa1906d084a7f973f1551991db82
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2025-09-06 22:00:52 -0400

    Add publish contract

--- de-news/projects/de-news-contracts/smart_contracts/hello_world/contract.py
+++ de-news/projects/de-news-contracts/smart_contracts/hello_world/contract.py
@@ -6,3 +6,9 @@ class HelloWorld(ARC4Contract):
     @abimethod()
     def hello(self, name: String) -> String:
         return "Hello, " + name
+
+
+class PublishNews(ARC4Contract):
+    @abimethod()
+    def publish(self, title: String, content: String) -> String:
+        return "Published news article: " + title + "\n" + content
